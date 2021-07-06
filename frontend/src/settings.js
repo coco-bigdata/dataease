18db5f495c5ef8b5a5739ab93c1f9791defcc8f1
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-07-06 16:09:50 +0800

    1. add docker compose

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -2,7 +2,7 @@ module.exports = {
   TokenKey: 'Authorization',
   RefreshTokenKey: 'refreshauthorization',
   LinkTokenKey: 'LINK-PWD-TOKEN',
-  title: 'DataEase',
+  title: '7otech',
 
   /**
    * @type {boolean} true | false
--- frontend/src/views/login/index.vue
+++ frontend/src/views/login/index.vue
@@ -12,7 +12,7 @@
               {{ uiInfo['ui.loginTitle'].paramValue }}
             </div>
             <div v-else class="login-welcome">
-              {{ $t('login.welcome') + (uiInfo && uiInfo['ui.title'] && uiInfo['ui.title'].paramValue || ' DataEase') }}
+              {{ $t('login.welcome') + (uiInfo && uiInfo['ui.title'] && uiInfo['ui.title'].paramValue || ' 7otech') }}
             </div>
             <div class="login-form">
               <el-form-item prop="username">
