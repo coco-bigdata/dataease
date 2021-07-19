f89214a22efed43ecdbc1dfa5ad7b871267b1d8f
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-07-19 12:04:30 +0800

    1. add docker doris

--- docker/doris-fe/entrypoint.sh
+++ docker/doris-fe/entrypoint.sh
@@ -3,11 +3,11 @@
 echo "fe_role:"$FE_ROLE
 echo "leader:"$FE_LEADER
 
-if [[ $FE_ROLE = 'fe-leader' ]]; then
+if [ $FE_ROLE = 'fe-leader' ]; then
     /home/doris/fe/bin/start_fe.sh
-elif [[ $FE_ROLE = 'be' ]]; then
+elif [ $FE_ROLE = 'be' ]; then
     /home/doris/be/bin/start_be.sh
-elif [[ $FE_ROLE = 'fe-follower' ]]; then
+elif [ $FE_ROLE = 'fe-follower' ]; then
     /home/doris/fe/bin/start_fe.sh --helper $FE_LEADER
 else
     /home/doris/fs_broker/bin/start_broker.sh
